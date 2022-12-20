59e56f4cb5feae43bc68b2d421ac83f835c34c31
Uche Ibute <uchennaoluchi49@gmail.com>
2022-12-20 21:22:31 +0100

    Fixing the last two requirments

--- java.js
+++ java.js
@@ -1,9 +1,16 @@
 const bar = document.querySelector(".fa-bars");
 const menu = document.querySelector(".dropdown-menu");
 const mark = document.querySelector(".xmark");
+const getBody = document.body;
+const Items = document.querySelectorAll(".menu .ax");
 bar.addEventListener("click", () => {
   menu.classList.toggle("hide");
 });
 mark.addEventListener("click", () => {
   menu.classList.toggle("hide");
+});
+Items.forEach((n) => {
+  n.addEventListener('click', () => {
+    menu.classList.toggle('hide');
+  });
 });
