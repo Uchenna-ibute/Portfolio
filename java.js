71ea0cd0c67f6ab66db4a42fdced1a0333220fe5
Uche Ibute <uchennaoluchi49@gmail.com>
2022-12-20 21:34:18 +0100

    Fixing the errors

--- java.js
+++ java.js
@@ -6,9 +6,7 @@ const Items = document.querySelectorAll(".menu .ax");
 bar.addEventListener("click", () => {
   menu.classList.toggle("hide");
 });
-mark.addEventListener("click", () => {
-  menu.classList.toggle("hide");
-});
+
 Items.forEach((n) => {
   n.addEventListener('click', () => {
     menu.classList.toggle('hide');
