99cd7ef8465e5852c13dd60bd9fcae8b5918669c
Uche Ibute <uchennaoluchi49@gmail.com>
2022-12-22 23:09:53 +0100

    introduction to popup

--- java.js
+++ java.js
@@ -13,3 +13,60 @@ Items.forEach((n) => {
     menu.classList.toggle('hide');
   });
 });
+const projectDetails = [{
+  name: 'Keeping track of hundreds  of components website',
+  name2: 'Multi Post Stories',
+  detail: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.',
+  detail2: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
+  image: './img/Snapshoot.png',
+  technologies: [
+    'HTML/CSS',
+    'Ruby on Rails',
+    'JavaScript',
+  ],
+  live: 'https://github.com/Uchenna-ibute/Portfolio',
+  source: 'https://github.com/Uchenna-ibute',
+}
+];
+
+const projectCards = document.querySelectorAll('.project-cards');
+const getModal = document.querySelector('#modal');
+const pjName = getModal.querySelector('.name');
+const pjName2 = getModal.querySelector('.name2');
+const lgLists = getModal.querySelectorAll('.lg-lt li');
+const mainImg = getModal.querySelector('.m-img');
+const imgLists = getModal.querySelectorAll('.img-lt li');
+const modalContent = getModal.querySelector('.content');
+const modalContent2 = getModal.querySelector('.content2');
+const liveLink = getModal.querySelector('#liveLink');
+const sourceLink = getModal.querySelector('#sourceLink');
+const closeBtn = getModal.querySelector('.close-btn');
+
+for (let i = 0; i < projectCards.length; i += 1) {
+  const pjbtn = projectCards[i].querySelector('.pjbtn');
+  const project = projectDetails[i];
+
+  pjbtn.addEventListener('click', () => {
+    getModal.classList.add('active');
+
+    pjName.innerText = project.name;
+    pjName2.innerText = project.name2;
+    mainImg.style.backgroundImage = `url(${project.image})`;
+    modalContent.innerText = project.detail;
+    modalContent2.innerText = project.detail2;
+    liveLink.href = project.live;
+    sourceLink.href = project.source;
+
+    for (let j = 0; j < lgLists.length; j += 1) {
+      lgLists[j].innerText = project.technologies[j];
+    }
+
+    imgLists.forEach((imgList) => {
+      imgList.style.backgroundImage = `url(${project.image})`;
+    });
+
+    closeBtn.addEventListener('click', () => {
+      getModal.classList.remove('active');
+    });
+  });
+}
